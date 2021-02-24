030124029b442546122f9028386b6d09039659ad
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-02-24 09:13:28 -0800

    express implementation

--- functions/index.js
+++ functions/index.js
@@ -3,22 +3,39 @@ const admin = require('firebase-admin')
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello world!");
-});
+const express = require('express')
+const app = express()
 
-
-exports.getScreams = functions.https.onRequest((req, res) => {
+app.get('/screams', (req, res) => {
     admin.firestore().collection('screams').get()
         .then(data => {
             let screams = []
             data.forEach(doc => {
-                screams.push(doc.data())
+                screams.push({
+                    screamId: doc.id,
+                    ...doc.data()
+                })
             })
             return res.json(screams);
         })
         .catch(err => console.log(err))
 })
+
+app.post('/scream', (req, res) => {
+    const newScream = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    }
+
+    admin.firestore().collection('screams').add(newScream)
+        .then((doc) => {
+            res.json({ message: `document ${doc.id} created successfully` })
+        })
+        .catch((err) => {
+            res.status(500).json({ error: 'something went wrong' })
+            console.log(err)
+        })
+})
+
+exports.api = functions.https.onRequest(app);
